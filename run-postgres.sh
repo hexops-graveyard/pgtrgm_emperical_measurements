fdec89cf2fc89ff04516b4d47ed63daf99c435e1
Stephen Gutekanst <stephen@hexops.com>
2021-02-08 14:44:36 -0700

    configuration 3
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- run-postgres.sh
+++ run-postgres.sh
@@ -6,4 +6,7 @@ docker run --name postgres \
     -e POSTGRES_HOST_AUTH_METHOD=trust \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
     -v "$PWD/postgres.conf":/etc/postgresql/postgresql.conf \
+    --shm-size=2g \
     postgres:13.1-alpine postgres -c 'config_file=/etc/postgresql/postgresql.conf'
+
+# --shm-size explanation: https://stackoverflow.com/a/56754077
